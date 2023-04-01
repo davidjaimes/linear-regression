ebcc3b9f3049d8b9389a5b1733cb0bb2932bc8ef
David Jaimes <dj@davidjaimes.com>
2023-04-01 11:22:46 -0700

    Create mpl_style.py

---
+++ mpl_style.py
@@ -0,0 +1,61 @@
+pro = {
+    # Patches
+    'patch.linewidth': 0.0,
+    'patch.facecolor': 'w',
+
+    # Lines
+    'lines.linewidth': 2.5,
+    'lines.color': 'deepskyblue',
+
+    # Font
+    'font.size': 18.0,
+
+    # Axes
+    'axes.facecolor': 'black',
+    'axes.edgecolor': 'C7',
+    'axes.linewidth': 0.3,
+    'axes.grid': True,
+    'axes.grid.axis': 'y',
+    'axes.grid.which': 'major',
+    'axes.labelcolor': 'C7',
+    'axes.axisbelow': True,
+    'axes.titlepad': 20.0,
+    'axes.titlecolor': 'C7',
+    'axes.titlelocation': 'left',
+    'axes.spines.top': False,
+    'axes.spines.right': False,
+    'axes.spines.left': False,
+
+    # X-Ticks
+    'xtick.labelsize': 12.0,
+    'xtick.major.size': 5,
+    'xtick.color': 'C7',
+
+    # Y-Ticks
+    'ytick.labelsize': 12.0,
+    'ytick.major.size': 0.0,
+    'ytick.color': 'C7',
+    'ytick.major.pad': 2.0,
+
+    # Grid
+    'grid.color': 'C7',
+    'grid.linestyle': 'solid',
+    'grid.linewidth': 0.3,
+
+    # Legend
+    'legend.facecolor': 'whitesmoke',
+    'legend.framealpha': 1.0,
+    'legend.fontsize': 12.0,
+
+    # Figure
+    'figure.figsize': (8, 4),
+    'figure.facecolor': 'black',
+
+    # Errorbar Plots
+    'errorbar.capsize': 3.0,
+
+    # Other
+    'savefig.dpi': 300,
+    'savefig.transparent': True,
+    'savefig.bbox': 'tight',
+}
